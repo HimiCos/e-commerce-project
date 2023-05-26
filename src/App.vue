7025fc3c086191bbcd717b85edec47889a02e81f
HimiCos <ineewhite@gmail.com>
2023-05-26 11:39:01 +0800

    1

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="box">
+    <h1>我是app組件</h1>
+  </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped lang="scss">
+.box {
+  width: 600px;
+  height: 600px;
+  background-color: black;
+  h1 {
+    color: white;
+  }
+}
+</style>
