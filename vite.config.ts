38d3defe7895283f21eb952b13c4044694443533
HimiCos <ineewhite@gmail.com>
2023-06-04 21:56:37 +0800

    day: 10

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,16 @@
+// https://vitejs.dev/config/
 import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import path from 'path'
-// 引入svg需要用到的插件
+//引入svg需要用到插件
 import { createSvgIconsPlugin } from 'vite-plugin-svg-icons'
-// 引入mock插件提供的方法 viteMockServe
+//mock插件提供方法
 import { viteMockServe } from 'vite-plugin-mock'
 export default defineConfig(({ command, mode }) => {
-  // 獲取各种環境下对应的變量
-  let env = loadEnv(mode, process.cwd())
+  //获取各种环境下的对应的变量
+  const env = loadEnv(mode, process.cwd())
   return {
+    publicPath: 'https://gitee.com/jch1011/guiguzhenxuan',
     plugins: [
       vue(),
       createSvgIconsPlugin({
@@ -16,7 +18,7 @@ export default defineConfig(({ command, mode }) => {
         symbolId: 'icon-[dir]-[name]',
       }),
       viteMockServe({
-        localEnabled: command === 'serve', // 保證開發階段可以使用mock接口
+        localEnabled: command === 'serve', //保证开发阶段可以使用mock接口
       }),
     ],
     resolve: {
@@ -24,7 +26,7 @@ export default defineConfig(({ command, mode }) => {
         '@': path.resolve('./src'), // 相对路径别名配置，使用 @ 代替 src
       },
     },
-    // scss全局變量配置
+    //scss全局变量一个配置
     css: {
       preprocessorOptions: {
         scss: {
@@ -33,14 +35,15 @@ export default defineConfig(({ command, mode }) => {
         },
       },
     },
+    //代理跨域
     server: {
       proxy: {
         [env.VITE_APP_BASE_API]: {
-          // 获取环境变量中的代理地址
+          //获取数据的服务器地址设置
           target: env.VITE_SERVE,
-          // 允许跨域
+          //需要代理跨域
           changeOrigin: true,
-          // 重写路径
+          //路径重写
           rewrite: (path) => path.replace(/^\/api/, ''),
         },
       },
